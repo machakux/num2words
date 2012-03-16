ae6955cb44b9324a292de55551af1a1f893ae6ca
machaku <bmachaku@gmail.com>
2012-03-16 20:30:33 +0300

    fixes

--- num2words.py
+++ num2words.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
+__author__  = (
+	'Machaku Banga',
+	)
+
+__license__ = 'Apache License, 2.0 (Apache-2.0)'
+__version__ = '2012.03.16'
+
 import sys
+import re
 
 ones=['','one','two','three','four','five','six','seven','eight','nine',]
-teens=['','eleven','twelve','thirteen','fourteen','fifteen','sixteen','eighteen','nineteen']
+teens=['','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
 tens=['','ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety']
-hundreds=['','one hundred','two hundred','three hunddred','four hundred','five hundred','six hundred','seven hundred','eight hundred','nine hundred']
+hundreds=['','one hundred','two hundred','three hundred','four hundred','five hundred','six hundred','seven hundred','eight hundred','nine hundred']
 rank=['','thousand','million','billion','trillion','quadrillion','quintillion','sextillion','septillion','octillion','nonillion','decillion','undecillion','duodecillion','tredecillion','quattuordecillion','quindecillion','sexdecillion','septendecillion','octodecillion','novemdecillion','vigintillion']
 
 
@@ -38,8 +46,6 @@ class Number:
 		if number < 0:
 			number -= number
 			word += 'negative '
-		if number==0:
-			word += 'zero'
 		if number <1000:
 			if number >= 100:
 				hundred = number//100
@@ -51,7 +57,13 @@ class Number:
 					else:
 						ten = hundredr//10
 						one = hundredr%10
-					word += hundreds[hundred] +' and '+ tens[ten]+' '+ones[one]
+					word += hundreds[hundred] 
+					if ten:
+						word+= ' and '+ tens[ten]
+					if one and ten:
+						word += ' '+ones[one]
+					if one and not ten:
+						word += ' and '+ones[one]
 				else:
 					word += hundreds[hundred]
 			if 100> number >= 10:
@@ -61,7 +73,9 @@ class Number:
 				else:
 					ten = number//10
 					one = number%10
-					word += tens[ten]+' '+ones[one]
+					word += tens[ten]
+					if one:
+						word += ' '+ones[one]
 			elif number<10:
 				word += ones[number]
 				
@@ -70,10 +84,40 @@ class Number:
 	def convert_to_words_order(self,number):
 		order = self.get_order(number)
 		hundred = number//pow(10,3*order)
-		return self.convert_to_words_hundreds(hundred) +' '+rank[order]+','
+		return self.convert_to_words_hundreds(hundred) +' '+rank[order]
+		
+	def convert_to_digits(self,snumber):
+		word=''
+		word_l=[]
+		digits=list(snumber)
+		for i in digits:
+			if int(i)==0:
+				word_l.append('zero')
+			else:
+				word_l.append(ones[int(i)])
+		word=' '.join(word_l)
+		return word
+	
+	def get_fraction_digits(self,integer=False):
+		number_s=str(self.number)
+		try:
+			number_ls = re.split('[.]',number_s)
+			digits = number_ls[1]
+			if integer:
+				digits = int(digits)
+		except:
+			digits = False
+		return digits
 	
 	def convert_to_words(self):
 		word=''
+		fraction = self.get_fraction_digits()
+		try:
+			number_s=str(self.number)
+			number_ls = re.split('[.]',number_s)
+			self.number = int(number_ls[0])
+		except:
+			pass
 		if self.number < 0:
 			self.number = -self.number
 			word += 'negative '
@@ -82,9 +126,8 @@ class Number:
 		number = self.number
 		if number < 1000:
 			word += self.convert_to_words_hundreds(number)
-			word += '.'
 		else:
-			if number%10000<=999:
+			if number%1000:
 				terminator =' and '
 			else:
 				terminator =''
@@ -93,21 +136,26 @@ class Number:
 				digits_in_order = number//pow(10,3*order)
 				value_in_order = (digits_in_order * pow(10,3*order))
 				word += self.convert_to_words_order(number)
-				if order >= 1:
-					word += ' '
-
 				number = number - value_in_order
+				if number:
+					word += ','
+				if order >= 1 and number:
+					word += ' '
 			else:
 				if terminator:
 					word = word[:-2]
 				word+=terminator
 				word += self.convert_to_words_hundreds(number)
-			word+='.'
+		if fraction:
+			word+= ' point '+self.convert_to_digits(fraction)
 		return word
 
 try:
 	for i in range(1,len(sys.argv)):
-		no = Number(int(sys.argv[i]))
+		try:
+			no = Number(int(sys.argv[i]))
+		except ValueError:
+			no = Number(float(sys.argv[i]))
 		print (no.convert_to_words())
 except KeyError:
 	pass
